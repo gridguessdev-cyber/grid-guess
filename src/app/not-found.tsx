eb5391e0d579925335ae237b6eaca5e3426792c5
Danik499 <danikshcherban499@gmail.com>
2025-10-16 11:33:13 +0300

    Added not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function GlobalNotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
+      <p className="text-2xl text-gray-600">Page Not Found</p>
+    </div>
+  );
+}
