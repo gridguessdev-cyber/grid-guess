2881e6d9f4bbb6e8b85e53ed4655e6bbdf14ad7f
Danik499 <danikshcherban499@gmail.com>
2025-10-27 13:52:09 +0200

    Added filter by country

--- src/components/LevelsList/index.tsx
+++ src/components/LevelsList/index.tsx
@@ -6,41 +6,73 @@ import _ from "lodash";
 import Image from "next/image";
 import { motion } from "motion/react";
 import { useServices } from "@/providers/ServicesProvider";
+import { maps } from "@/data/exports";
+import { useState } from "react";
 
 export default function LevelsList() {
+  const [selectedFilterCountry, setSelectedFilterCountry] =
+    useState<string>("All");
   const { levelsService } = useServices();
 
-  const { data: levels } = useQuery({
-    queryKey: ["levels"],
-    queryFn: () => levelsService.getLevels(),
+  const { data: levels, isLoading } = useQuery({
+    queryKey: ["levels", selectedFilterCountry],
+    queryFn: () => levelsService.getLevels({ map: selectedFilterCountry }),
   });
 
-  if (!levels) return;
+  const mapsList = Object.keys(maps).map(_.startCase);
+  mapsList.unshift("All");
 
   return (
-    <motion.div
-      initial={{ y: 20, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.5, ease: "easeOut" }}
-    >
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        {levels.map((level) => (
-          <Link key={level.id} href={`/levels/${level.id}`}>
-            <div className="rounded-xl p-4 flex justify-between items-center bg-white">
-              <div className="alfa-slab-one-regular">{level.country}</div>
-              <div className="flex gap-2 items-center">
-                <Image
-                  src={"/icons/world-map.svg"}
-                  alt="map icon"
-                  width={30}
-                  height={30}
-                />
-                <div className="font-bold">{_.capitalize(level.map)}</div>
-              </div>
-            </div>
-          </Link>
-        ))}
+    <div className="flex flex-col gap-5">
+      <div className="flex justify-end">
+        <span>Filter by region: </span>
+        <select
+          value={selectedFilterCountry}
+          onChange={(event) => {
+            setSelectedFilterCountry(event.target.value);
+          }}
+        >
+          {mapsList.map((item) => (
+            <option key={item}>{item}</option>
+          ))}
+        </select>
       </div>
-    </motion.div>
+      {!isLoading && !levels?.length && (
+        <motion.div
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          className="text-center mt-3 font-semibold"
+        >
+          No levels found for the selected region.
+        </motion.div>
+      )}
+      {!!levels?.length && (
+        <motion.div
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {levels.map((level) => (
+              <Link key={level.id} href={`/levels/${level.id}`}>
+                <div className="rounded-xl p-4 flex justify-between items-center bg-white">
+                  <div className="alfa-slab-one-regular">{level.country}</div>
+                  <div className="flex gap-2 items-center">
+                    <Image
+                      src={"/icons/world-map.svg"}
+                      alt="map icon"
+                      width={30}
+                      height={30}
+                    />
+                    <div className="font-bold">{_.capitalize(level.map)}</div>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </motion.div>
+      )}
+    </div>
   );
 }
--- src/lib/LevelsService.ts
+++ src/lib/LevelsService.ts
@@ -2,12 +2,28 @@ import { CreateLevelParams, Level } from "@/types/api";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { Database } from "../../database.types";
 
+type GetLevelsParams = {
+  map: string;
+};
+
 export class LevelsService {
   constructor(private client: SupabaseClient<Database>) {}
 
-  async getLevels(): Promise<Level[]> {
+  async getLevels(params?: GetLevelsParams): Promise<Level[]> {
     try {
-      const levels = (await this.client.from("levels").select("*")).data!;
+      let levels;
+
+      if (params?.map && params.map !== "All") {
+        levels = (
+          await this.client
+            .from("levels")
+            .select("*")
+            .eq("map", params.map.toLowerCase())
+        ).data!;
+      } else {
+        levels = (await this.client.from("levels").select("*")).data!;
+      }
+
       return levels;
     } catch (error) {
       console.error("Error fetching levels:", error);
